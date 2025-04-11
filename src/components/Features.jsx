53c6cd2afac7004b3059d8959f6b382093fbfc2c
MuhammadAsad20 <121595889+MuhammadAsad20@users.noreply.github.com>
2025-04-11 01:20:18 -0700

    Add files via upload

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import MemberIcon from '../assets/images/MemberIcon.png';
+import Members from '../assets/images/Members.png';
 import BuildingIcon from '../assets/images/BuildingIcon.png';
 import GroupIcon from '../assets/images/GroupIcon.png';
 
@@ -20,7 +20,7 @@ const Features = () => {
           <div className="xl:w-1/3 md:w-1/2 p-4">
             <div className="border border-gray-200 p-6 rounded-lg text-center">
               <div className="w-14 h-14 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-              <img src={MemberIcon} alt="" />
+              <img src={Members} alt="" />
               </div>
               <h2 className="text-lg text-gray-900 font-medium title-font mb-2">Membership Organisations</h2>
               <p className="leading-relaxed text-base">
